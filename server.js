2e57588e63269aa85579f59fc6f39549eefd4d8e
Nadav <nadavnoy@yahoo.com>
2019-03-16 17:51:21 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+// BodyParser middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+
+mongoose
+  .connect(db, { useNewUrlParser: true} )
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Use routes
+app.use('/api/items', items);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// Connect to port
+const port = process.env.port || 5000;
+
+app.listen(port, () => console.log(`server began on port ${port}`));
